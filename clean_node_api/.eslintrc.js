be84d3d77aa8c1807baaef2683fa25ad4b910fd5
Joberth Rogers <joberth.rogers18@gmail.com>
2020-06-18 23:27:33 -0300

    chore: add lint setup

---
+++ clean_node_api/.eslintrc.js
@@ -0,0 +1,21 @@
+/* eslint-disable semi */
+/* eslint-disable comma-dangle */
+/* eslint-disable quotes */
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  env: {
+    es6: true,
+    node: true,
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["plugin:@typescript-eslint/recommended", "standard"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  rules: {},
+};
