7666b0888a71dacb98c0044d0b9a72f20427f813
Joberth Rogers <joberth.rogers18@gmail.com>
2020-06-23 21:54:35 -0300

    test: use jest mock to change the return of method to test the possibility false to email validation

--- clean_node_api/src/presentation/controllers/signup.spec.ts
+++ clean_node_api/src/presentation/controllers/signup.spec.ts
@@ -1,10 +1,36 @@
 import { SignUpController } from './signup'
 import { MissingParamError } from '../errors/missing-params-error'
+import { InvalidParamError } from '../errors/invalid-params-erros'
+import { EmailValidator } from '../protocols/email-validator'
+
+interface SutTypes {
+  sut: SignUpController;
+  emailValidatorStub: EmailValidator;
+}
+
+// "factory", avoid the maintenance in multiple parts
+// of code, join in a unique place
+const makeSut = (): SutTypes => {
+  // stub test
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+
+  const emailValidatorStub = new EmailValidatorStub()
+  const sut = new SignUpController(emailValidatorStub)
+
+  return {
+    sut,
+    emailValidatorStub
+  }
+}
 
 describe('SignUp controller', () => {
   test('Should return 400 if no name provided', () => {
     // sut -> system under test
-    const sut = new SignUpController()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@mail.com',
@@ -20,7 +46,7 @@ describe('SignUp controller', () => {
 
   test('Should return 400 if no email provided', () => {
     // sut -> system under test
-    const sut = new SignUpController()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -36,7 +62,7 @@ describe('SignUp controller', () => {
 
   test('Should return 400 if no password is provided', () => {
     // sut -> system under test
-    const sut = new SignUpController()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@mail.com',
@@ -52,7 +78,7 @@ describe('SignUp controller', () => {
 
   test('Should return 400 if no password confimation provided', () => {
     // sut -> system under test
-    const sut = new SignUpController()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@mail.com',
@@ -64,5 +90,26 @@ describe('SignUp controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
+  })
+
+  test('Should return 400 if an invalid email is provided', () => {
+    // sut -> system under test
+    const { sut, emailValidatorStub } = makeSut()
+
+    // use the jest to change the return of method to expected of result from this test
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+
+    const httpRequest = {
+      body: {
+        email: 'invalid_email@mail.com',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('email'))
   })
 })
