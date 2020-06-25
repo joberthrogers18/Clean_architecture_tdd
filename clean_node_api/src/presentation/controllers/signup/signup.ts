e47fad0066d5e6afa86c0d7754baabe78d322c72
Joberth Rogers <joberth.rogers18@gmail.com>
2020-06-25 00:13:25 -0300

    feat: ensure SignUpController return object User when everything is allright

--- clean_node_api/src/presentation/controllers/signup/signup.ts
+++ clean_node_api/src/presentation/controllers/signup/signup.ts
@@ -31,7 +31,11 @@ export class SignUpController implements Controller {
         return badRequest(new InvalidParamError('email'))
       }
 
-      this.addAccount.add({ name, email, password })
+      const account = this.addAccount.add({ name, email, password })
+      return {
+        statusCode: 200,
+        body: account
+      }
     } catch (err) {
       return internalServerError()
     }
