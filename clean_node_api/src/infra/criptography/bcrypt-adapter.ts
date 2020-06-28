6e2badd8e9e50595046c529fffa57df89707a652
Joberth Rogers <joberth.rogers18@gmail.com>
2020-06-28 19:22:45 -0300

    feat: ensure Bcrypt return hash value when it works

--- clean_node_api/src/infra/criptography/bcrypt-adapter.ts
+++ clean_node_api/src/infra/criptography/bcrypt-adapter.ts
@@ -8,7 +8,7 @@ export class BcryptAdapter implements Encrypter {
   }
 
   async encrypt (value: string): Promise<string> {
-    await bcrypt.hash(value, this.saltEncrypt)
-    return null
+    const hash = await bcrypt.hash(value, this.saltEncrypt)
+    return hash
   }
 }
--- clean_node_api/src/infra/criptography/bcrypt-adpter.spec.ts
+++ clean_node_api/src/infra/criptography/bcrypt-adpter.spec.ts
@@ -1,12 +1,42 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
+// mock implementation bcrypt to return a fixed hash
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return new Promise(resolve => resolve('hash'))
+  }
+}))
+
+const makeBcryptAdapter = (saltEncrypt: number): BcryptAdapter => {
+  return new BcryptAdapter(saltEncrypt)
+}
+
+interface SutType {
+  bcryptAdapterSut: BcryptAdapter
+}
+
+const makeSut = (saltEncrypt: number): SutType => {
+  const bcryptAdapterSut = makeBcryptAdapter(saltEncrypt)
+
+  return {
+    bcryptAdapterSut
+  }
+}
+
 describe('Bcrypt Adapter', () => {
   test('Should call bcrypt with correct values', async () => {
     const saltEncrypt = 12
-    const sut = new BcryptAdapter(saltEncrypt)
+    const { bcryptAdapterSut } = makeSut(saltEncrypt)
     const hashSpy = jest.spyOn(bcrypt, 'hash')
-    await sut.encrypt('any_value')
+    await bcryptAdapterSut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', saltEncrypt)
+  })
+
+  test('Should call hashed string if bcrypt works', async () => {
+    const saltEncrypt = 12
+    const { bcryptAdapterSut } = makeSut(saltEncrypt)
+    const hash = await bcryptAdapterSut.encrypt('any_value')
+    expect(hash).toBe('hash')
   })
 })
