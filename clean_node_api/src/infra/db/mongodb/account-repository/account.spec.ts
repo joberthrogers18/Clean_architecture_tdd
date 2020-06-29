29ae336de27b95baccd0728c19dd9598b738cfaa
Joberth Rogers <joberth.rogers18@gmail.com>
2020-06-28 22:16:29 -0300

    feat: add mongo helper to instace the mongodb client

---
+++ clean_node_api/src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,31 @@
+import { MongoHelpers } from '../helpers/mongo-helpers'
+
+describe('Account Mongo', () => {
+  beforeAll(async () => {
+    await MongoHelpers.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelpers.disconnect()
+  })
+
+  test('should test', () => {
+    expect(1).toBe(1)
+  })
+
+  //  test('Should return an account when success', async () => {
+  //     const sut = new AccountMongoRepository()
+  //     const account = await sut.add({
+  //       name: 'any_name',
+  //       email: 'any_email@mail.com',
+  //       password: 'any_password'
+  //     })
+
+  //     // it is not undefined
+  //     expect(account).toBeTruthy()
+  //     expect(account.id).toBeTruthy()
+  //     expect(account.name).toBe('any_name')
+  //     expect(account.email).toBe('any_email@mail.com')
+  //     expect(account.password).toBe('any_password')
+  //   });
+})
